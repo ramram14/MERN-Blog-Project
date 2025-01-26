07b139abd695ce8f406a2db07ec30af94aa877ea
Ikram <ikrammuh1402@gmail.com>
2025-01-26 08:23:10 +0700

    add url base api correct

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'https://vercel.com/ikrams-projects-e50d00ad/write-sphere-backend/8Lqec96Vcy3PniM64HNMng2iMb8Q',
+  baseURL: 'https://write-sphere-backend.vercel.app',
   withCredentials: true
 })
